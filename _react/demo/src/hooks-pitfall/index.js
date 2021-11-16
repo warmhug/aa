a15b1d36c747798f171209db991eb8fbefd1795e
然则 <hualei.hl@alibaba-inc.com>
2021-11-16 16:44:56 +0800

    autocommit

--- _react/demo/src/hooks-pitfall/index.js
+++ _react/demo/src/hooks-pitfall/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Debounce from './Debounce';
+import Throttle from './Throttle';
 import Es from './EventState';
 import Timer from './Timer';
 
 const Index = () => {
   return (
     <div>
+      <Debounce />
+      <Throttle />
       <Es />
       <Timer />
     </div>
